a4a34f48e62092f12f1fad18a172b65a60fbb9e1
CarmenIdalia <caguevara@unitru.edu.pe>
2024-09-29 22:19:38 -0500

    feat: agregación de adminLTE

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,26 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('adminlte::page')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
-            </div>
-        </div>
+
+@section('content_header')
+    <h1 ><i class="fas fa-utensils"></i>
+    </i>BIENVENIDOS</h1>
+
+@stop
+
+@section('content')
+    <div class="text-center mb-4">
+        <img src="vendor/adminlte/dist/img/Fondo1.jpeg" alt="logo" style="width: 1150px; height: 500px;">
     </div>
-</x-app-layout>
+@stop
+
+
+
+
+@section('css')
+    {{-- Add here extra stylesheets --}}
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@stop
+
+@section('js')
+    <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
+@stop
